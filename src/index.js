8bc870199435836f718e258037ee5a809b40cecb
cuong <cuong98nguyen@gmail.com>
2021-10-04 00:05:10 +0700

    set up project

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const path = require('path')
+var morgan = require('morgan')
+var handlebars  = require('express-handlebars')
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars({extname: 'hbs'}));
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+// console.log('path: '+path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
